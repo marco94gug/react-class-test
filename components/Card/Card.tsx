fc869f3022777b1ffadbce634eb9464a1cfdd81e
marco94gug <marco.guglielmino94@gmail.com>
2023-01-07 18:36:29 +0100

    feat: test fetch call for vercel

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -19,6 +19,14 @@ export default class Card extends React.Component<
     this.setState({ title: "Second Title" });
   }
 
+  componentDidMount(): void {
+    fetch(
+      "http://ddragon.leagueoflegends.com/cdn/12.22.1/data/en_US/champion.json"
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(Object.values(data.data)));
+  }
+
   render() {
     return (
       <div className={styles.main_card}>
